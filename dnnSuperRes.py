762338c89e1592d3df3ad3c686fd6747767856a3
Stephano Robinson <rashan.stephano@gmail.com>
2021-03-28 19:29:18 +0200

    V1 TEST

---
+++ dnnSuperRes.py
@@ -0,0 +1,27 @@
+import cv2
+import recordVideo
+imageName: str = "./bird2.JPG"
+outputName: str = "./upscaled.png"
+path: str = "models/EDSR_x4.pb"
+model: str = "edsr"
+model_scale: int = 4
+
+
+def upscale_image(image_name, model_path, model_name, scale, output_name):
+    # Create an SR object - only function that differs from c++ code
+    sr = cv2.dnn_superres.DnnSuperResImpl_create()
+    # Read image
+    image = cv2.imread(image_name)
+    # Read the desired model
+
+    sr.readModel(model_path)
+    # Set the desired model and scale to get correct pre- and post-processing
+    sr.setModel(model_name, scale)
+    # Upscale the image
+    result = sr.upsample(image)
+    # Save the image
+    cv2.imwrite(output_name, result)
+
+
+
+
---
+++ recordVideo.py
@@ -0,0 +1,83 @@
+#import numpy as np
+import os
+import cv2
+
+fileName = 'video.avi'
+fileNameNew = ''
+fps = 25.0
+resolution = '720p'
+
+
+def change_resolution(cap, width, height):
+    cap.set(3, width)
+    cap.set(4, height)
+
+
+STD_DIMENSIONS = {
+    "480p": (640, 480),
+    "720p": (1280, 720),
+    "1080p": (1920, 1080),
+    "4k": (3840, 2160)
+}
+
+
+VIDEO_TYPE = {
+    'avi': cv2.VideoWriter_fourcc(*'H264'),
+    'mp4': cv2.VideoWriter_fourcc(*'XVID')
+}
+"""def searchExt(fileName):
+    for i in len(fileName):
+        arrayFileName = fileName[i]
+    if '.' in fileName:
+        for i in [4, 3, 2]:
+            ext =+ fileName[i]
+        return  ext
+    return 'ext'"""
+
+
+def get_video_type(fileName):
+    filename, ext = os.path.splitext(fileName)
+    if ext in VIDEO_TYPE:
+        return VIDEO_TYPE[ext]
+    return VIDEO_TYPE['avi']
+
+
+def get_dims(cap, res="1080p"):
+    width, height = STD_DIMENSIONS["480p"]
+    if res in STD_DIMENSIONS:
+        width, height = STD_DIMENSIONS[res]
+    change_resolution(cap, width, height)
+    return width, height
+
+"""def checkFIleName(fileName):
+    if os.path.isfile(fileName):
+        fileNameNew = 'video.avi'
+        print("file exist, new file created : "+fileName)"""
+
+
+def recVideo(fileName, resolution):
+    """créer un objet capturant l'appareil webcam par défaut"""
+    cap = cv2.VideoCapture(0)
+    dim = get_dims(cap, res=resolution)
+    videoTypeCV2 = get_video_type(fileName)
+
+    out = cv2.VideoWriter(fileName, videoTypeCV2, fps, dim)
+
+    """dans cv2, set -> 3 = width et 4 = height"""
+    """Loop permettant l'utilisation continue"""
+
+    while True:
+        """lire frame par frame"""
+        ret, frame = cap.read()
+        """gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)"""
+        out.write(frame)
+
+        """afficher cette image sur l'ordi"""
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
+    """release"""
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()

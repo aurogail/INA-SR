2d15e17a35a489c50856694feb0886d3794d88fb
Aurore Gailhard <gailhardaurore@hotmail.fr>
2021-04-04 15:49:58 +0200

    gui

--- GUI.py
+++ GUI.py
@@ -5,17 +5,6 @@ import os
 import tkinter as tk
 from PIL import Image, ImageTk
 
-# Function for openeing the file explorer
-def browseFiles ():
-    filename = filedialog.askopenfilenames(initialdir = "/",
-                                          title = "Selectionner un Fichier",
-                                          filetypes = (("Image files", ".jpg"),
-                                                       ("Image files",".png")))
-    img = Image.open(filename)
-    photo = ImageTk.PhotoImage(img)
-    lbl= Label(imge=photo)
-    lbl.image= photo
-
 # create window
 window = Tk()
 
@@ -23,13 +12,30 @@ window = Tk()
 window.title("SR GUI")
 
 #size window
-window.geometry("500x500")
-window.minsize(300,300)
-window.config(background='white')
+window.geometry("840x600")
+window.minsize(640,400)
+window.maxsize(840,600)
+window.resizable(width = True, height = True)
+window.configure(background='white')
+
+# Function for openeing the file explorer
+def openFilename():
+    filename = filedialog.askopenfilename(title = "Selectionner un Fichier",
+                                           filetypes = (("JPEG File","*.jpg"),
+                                                        ("PNG File","*.png"),
+                                                        ("all files","*.*")))
+    return filename
+
+def open_img():
+    x = openFilename()
+    original_img = ImageTk.PhotoImage(Image.open(x))
+    panel = Label(window, image = original_img)
+    panel.image=original_img
+    panel.grid(row = 4)
 
 #button
-open_button = Button(window, text='Ouvrir un fichier', command = browseFiles)
-open_button.pack()
+open_button = Button(window, text='Ouvrir un fichier', command = open_img).grid(row = 1, columnspan = 4)
+exit_button = Button(window, text="exit", command = lambda : exit()).grid(row = 2, columnspan = 4)
 
 #execute window
 window.mainloop()
